5fa6f8099d38f4cb64d2eb0a8e7d9e38979e498d
unknown <davidjmstewart@gmail.com>
2025-01-13 12:47:42 +1000

    Updated the way the grid is drawn - now the main script tells the shader where the horizontal and vertical lines are, and the shader draws them accordingly.

--- electric_field_lines.gd
+++ electric_field_lines.gd
@@ -4,18 +4,37 @@ extends Node2D
 
 @export_group("Simulation properties")
 @export var grid_width: float = 100
-@export var grid_resolution: float = 100
+@export var grid_resolution: int = 10
+
+#float horizontal_lines
 var d = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var f: ShaderMaterial = $Grid.material;
-	print(f)
+	var horizontal_lines = zero_out_array(512);
+	var vertical_lines = zero_out_array(512);
+
+	var step_width_x: float = 1.0/grid_resolution;
+	var grid_size = $Grid.transform.get_scale();
+	var step_width_y: float = step_width_x * grid_size.x / grid_size.y;
+	
+	for i in range(grid_resolution):
+		horizontal_lines[i] = i*step_width_y
+		vertical_lines[i] = i*step_width_x
+	#material.set_shader_param("horizontal_lines", horizontal_lines)
+	$Grid.material.set_shader_parameter("horizontal_lines_UV", horizontal_lines)
+	$Grid.material.set_shader_parameter("vertical_lines_UV", vertical_lines)
+	$Grid.material.set_shader_parameter("grid_resolution", grid_resolution)
+	$Grid.material.set_shader_parameter("resolution", grid_size)
+
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	d+=delta
-	$Grid.material.set_shader_parameter("grid_resolution", d);
-
 	pass
+	
+func zero_out_array(size: int) -> Array:
+	var array = []
+	array.resize(size)
+	array.fill(0.0)
+	return array
