f1115359b5b81f22f9267474f703ab663de77690
unknown <davidjmstewart@gmail.com>
2025-01-17 15:54:38 +1000

    Visual vectors now work.

--- visual_vector.gd
+++ visual_vector.gd
@@ -2,65 +2,58 @@
 class_name VisualVector
 extends Node2D
 
-var my_property: int = 1;
-var from: Vector2 = Vector2(1,0);
-var to: Vector2 = Vector2(0,1);
+var from: Vector2 = Vector2(100,100);
+var to: Vector2 = Vector2(200,200);
 
 @onready
 var VectorHead: Polygon2D = $VectorHead
 @onready
 var VectorLine: Line2D = $VectorLine
 
-var width = 50
-var pivot_point: Vector2;
+@export_range(0.1, 100, 0.1) var width: float = 50;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	# This section creates the vector head (a triangle) out of a polygon2d
 	# then rotates the head an places it at the end of the vector line
+	# Initially, the vector will be created pointing to the right
+	# i.e. the vector as constructed by this code, before rotating, will
+	# look like ---->
 
-
-	var a = Vector2(0,0)
-	var b = Vector2(width, 0)
-	var c = Vector2((a.x + b.x)/2, width)
+	# Calculate the 3 points of the triangle that from the VectorHead. 
+	# Make it horizontally centred at x = 0, y = 0
+	# This is an isoceles triangle where the base has width=width
+	var a = Vector2(0,-width/2,)
+	var b = Vector2(0,width/2,)
+	var c = Vector2(width, 0)
 	VectorHead.polygon = [a,b,c]
 	VectorHead.color = Color.GREEN
-	VectorLine.default_color = Color.GREEN;
-	VectorHead.position.x -= c.x
+
 	var vector_magnitude = (from - to).length();
+	
+	# the entire vector ----> needs to be as long as the vector
+	# calculated by from - to. The triangle head contributes
+	# to this length, so we need to subtract it from the vector
+	# magnitude to find how long the line portion needs to be
+	var vector_line_magnitude = vector_magnitude - width;
 	var angle = from.angle_to_point(to)
+
+	VectorLine.width = width/10
+	
+	VectorHead.position = Vector2(vector_line_magnitude, 0)
+	
 	var first_line_point = Vector2(0,0);
-	var second_line_point = Vector2(0, 0 - vector_magnitude)
+	var second_line_point = Vector2(vector_line_magnitude, 0)
 	
 	VectorLine.clear_points()
 	VectorLine.add_point(first_line_point)
 	VectorLine.add_point(second_line_point)
-	VectorLine.width = 1
-	self.position = from
-	print('Angle is %f' % angle)
-	print(from)
-	print(to)
-	#self.position.x -= c.x;
-	
-	#pivot_point =  Vector2(c.x, 0)
+	VectorLine.default_color = Color.GREEN;
 
-	#self.position = Vector2(400,300);
-	#$VectorHead.offset = pivot_point
-	
-	#$VectorHead.rotation = -angle
-	#$VectorHead.position = to
+	self.position = from;
+	self.rotation = angle;
 
-	pass # Replace with function body.
 
-func _draw() -> void:
-	draw_circle(pivot_point, 10, Color.PURPLE)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-
-	#$VectorHead.offset = pivot_point
-
-	#$VectorHead.rotate(delta/10.0)
-	queue_redraw();
-	pass
+#func _draw() -> void:
+	#draw_circle(pivot_point, 10, Color.PURPLE)
